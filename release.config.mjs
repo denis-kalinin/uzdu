48951dbcf028510a2249aef73430d19abea327cb
Denis Kalinin <dev@keenrouter.net>
2025-12-06 12:07:33 +0300

    ci: release.config init()

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,35 @@
+import { readFileSync, writeFileSync, cpSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+function init(){
+  const pkgRoot = "distTemp";
+  console.info(`Temporary distribution directory: ${pkgRoot}`);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const originalPackageJsonPath = resolve(__dirname, 'package.json');
+  const originalPackageJson = JSON.parse(readFileSync(originalPackageJsonPath, 'utf8'));
+  const { 
+    name, version, description, bin, type, main, types, 
+    keywords, author, repository, license, dependencies,
+    engines
+  } = originalPackageJson;
+  const publishConfig = originalPackageJson.publishConfig || {};
+  if(!publishConfig.tag) {
+      publishConfig.tag="latest";
+  }
+  const files = ["lib"];
+  const packageJson = {
+    name, version, description, bin, type, main, types, 
+    keywords, author, repository, license, dependencies,
+    engines, files
+  }
+  const buildDir = resolve(__dirname, "dist");
+  cpSync(buildDir, resolve(__dirname, pkgRoot, "lib"), { recursive: true });
+  writeFileSync(resolve(__dirname, pkgRoot, 'package.json'), JSON.stringify(packageJson, null, 2) + '\n', 'utf8');
+}
+init();
+
 /**
  * @type {{ plugins: (string | [string, {pkgRoot: string}])[]}}
  *         "@semantic-release/release-notes-generator"
@@ -8,8 +40,8 @@ export default {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/exec", {
-      "prepareCmd": "npm run release -- --env.nextVersion=${nextRelease.version}",
+      "prepareCmd": "npm run build -- --env.nextVersion=${nextRelease.version}",
     }],
-    ["@semantic-release/npm", {pkgRoot: "distTemp"}],
+    ["@semantic-release/npm", {pkgRoot}],
   ]
 };
